6e98894d9d02858dd3aaa3517a938eb29260c531
Putri Pratiwi <putripratiwis@gmail.com>
2022-10-13 21:23:38 +0700

    First Commit

---
+++ src/main/java/com/juaracoding/selenium/SeleniumOne.java
@@ -0,0 +1,48 @@
+package com.juaracoding.selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumOne {
+    public static void main(String[] args) throws InterruptedException {
+        // System Property for Chrome Driver
+        System.setProperty("webdriver.chrome.driver", "D:\\chromedriver.exe");
+
+        // Instantiate a ChromeDriver class.
+        WebDriver driver=new ChromeDriver();
+
+        driver.manage().window().maximize();
+
+        String url = "https://demoqa.com/text-box";
+
+        driver.get(url);
+
+        //String titleName = driver.getTitle();
+        //System.out.println(titleName);
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+
+        Thread.sleep(3000);
+
+        driver.findElement(By.id("userName")).sendKeys("JuaraCoding");
+        System.out.println("input username");
+        driver.findElement(By.id("userEmail")).sendKeys("info@juaracoding.com");
+        System.out.println("input email");
+        driver.findElement(By.id("currentAddress")).sendKeys("Jakarta");
+        System.out.println("input current address");
+        driver.findElement(By.id("permanentAddress")).sendKeys("Jakarta");
+        System.out.println("input permanent address");
+
+        //scroll vertical 1000 pixel
+        js.executeScript("window.scrollBy(0,3000");
+
+        driver.findElement(By.id("submit")).click();
+        System.out.println("button submit click");
+
+
+        //driver.quit();
+
+    }
+}
---
+++ src/main/java/com/juaracoding/selenium/SeleniumTwo.java
@@ -0,0 +1,83 @@
+package com.juaracoding.selenium;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class SeleniumTwo {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "D:\\chromedriver.exe");
+
+        // Instantiate a ChromeDriver class.
+        WebDriver driver=new ChromeDriver();
+
+        driver.manage().window().maximize();
+
+        String url = "https://demoqa.com/checkbox";
+
+        driver.get(url);
+
+        WebElement titleMenu = driver.findElement(By.xpath("//*[@id=\"app\"]/div/div/div[1]/div"));
+        System.out.println(titleMenu.getText());
+
+        driver.findElement(By.xpath("//*[@id=\"tree-node\"]/ol/li/span/label/span[3]")).click();
+        System.out.println("Home Checklist");
+
+        driver.findElement(By.xpath("//*[@id=\"tree-node\"]/ol/li/span/button")).click();
+        System.out.println("Button Toggle Clicked");
+        driver.findElement(By.xpath("//*[@id=\"tree-node\"]/ol/li/ol/li[1]/span/label/span[3]")).click();
+        System.out.println("Desktop UnChecklist");
+        driver.findElement(By.xpath("//*[@id=\"tree-node\"]/ol/li/ol/li[2]/span/label/span[3]")).click();
+        System.out.println("Documents UnChecklist");
+        driver.findElement(By.xpath("//*[@id=\"tree-node\"]/ol/li/ol/li[3]/span/label/span[3]")).click();
+        System.out.println("Downloads UnChecklist");
+
+        // Menu Radio Button
+        driver.findElement(By.id("item-2")).click();
+
+        WebElement titleMenuTwo = driver.findElement(By.xpath("//*[@id=\"app\"]/div/div/div[1]/div"));
+        System.out.println("Title Menu: "+ titleMenuTwo.getText());
+
+        driver.findElement(By.xpath("//*[@id=\"app\"]/div/div/div[2]/div[2]/div[2]/div[2]/label")).click();
+        System.out.println("Yes Radio Clicked");
+        driver.findElement(By.xpath("//*[@id=\"app\"]/div/div/div[2]/div[2]/div[2]/div[3]/label")).click();
+        System.out.println("Impressive Radio Clicked");
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        WebElement yesRadio = driver.findElement(By.id("yesRadio"));
+        WebElement impressiveRadio = driver.findElement(By.id("impressiveRadio"));
+        js.executeScript("arguments[0].click();", yesRadio);
+        System.out.println("Yes Radio Clicked use JavascriptExecutor");
+        js.executeScript("arguments[0].click();", impressiveRadio);
+        System.out.println("Impressive Radio Clicked use JavascriptExecutor");
+
+        driver.get("https://www.google.com");
+        System.out.println("Google");
+        WebElement inputSearch = driver.findElement(By.xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input"));
+        inputSearch.sendKeys("Wikipedia");
+        inputSearch.sendKeys(Keys.ENTER);
+
+        driver.navigate().back();
+        System.out.println("Navigate Back");
+
+        driver.get("https://demoqa.com/select-menu");
+        System.out.println("Select Menu");
+        js.executeScript("window.scrollBy(0,500)");
+        WebElement elementSelect = driver.findElement(By.id("oldSelectMenu"));
+        Select oldSelectMenu = new Select(elementSelect);
+        oldSelectMenu.selectByIndex(1);
+        oldSelectMenu.selectByValue("10");
+        oldSelectMenu.selectByVisibleText("Magenta");
+
+
+
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e){
+            throw new RuntimeException(e);
+        }
+
+
+    }
+}
